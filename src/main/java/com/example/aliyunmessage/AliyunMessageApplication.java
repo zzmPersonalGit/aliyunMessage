5b2b4b323be82b18dbb64afc9c908a6f2d0ef41a
zzm <qdssl12138@163.com>
2023-04-25 17:36:33 +0800

    Initial commit

---
+++ src/main/java/com/example/aliyunmessage/AliyunMessageApplication.java
@@ -0,0 +1,13 @@
+package com.example.aliyunmessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliyunMessageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AliyunMessageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aliyunmessage/SendSms.java
@@ -0,0 +1,80 @@
+package com.example.aliyunmessage;
+
+// This file is auto-generated, don't edit it. Thanks.
+
+import com.aliyun.auth.credentials.Credential;
+import com.aliyun.auth.credentials.provider.StaticCredentialProvider;
+import com.aliyun.sdk.service.dysmsapi20170525.models.*;
+import com.aliyun.sdk.service.dysmsapi20170525.*;
+import com.google.gson.Gson;
+import darabonba.core.client.ClientOverrideConfiguration;
+
+import java.util.concurrent.CompletableFuture;
+
+public class SendSms {
+    public static void main(String[] args) throws Exception {
+
+        // HttpClient Configuration
+        /*HttpClient httpClient = new ApacheAsyncHttpClientBuilder()
+                .connectionTimeout(Duration.ofSeconds(10)) // Set the connection timeout time, the default is 10 seconds
+                .responseTimeout(Duration.ofSeconds(10)) // Set the response timeout time, the default is 20 seconds
+                .maxConnections(128) // Set the connection pool size
+                .maxIdleTimeOut(Duration.ofSeconds(50)) // Set the connection pool timeout, the default is 30 seconds
+                // Configure the proxy
+                .proxy(new ProxyOptions(ProxyOptions.Type.HTTP, new InetSocketAddress("<your-proxy-hostname>", 9001))
+                        .setCredentials("<your-proxy-username>", "<your-proxy-password>"))
+                // If it is an https connection, you need to configure the certificate, or ignore the certificate(.ignoreSSL(true))
+                .x509TrustManagers(new X509TrustManager[]{})
+                .keyManagers(new KeyManager[]{})
+                .ignoreSSL(false)
+                .build();*/
+
+        // Configure Credentials authentication information, including ak, secret, token
+        StaticCredentialProvider provider = StaticCredentialProvider.create(Credential.builder()
+                .accessKeyId("LTAI5t6xeejPeA4Q3zAgaasp")
+                .accessKeySecret("xxMdN2zXAcbU9tv3wS3EfBH72MdELI")
+                //.securityToken("<your-token>") // use STS token
+                .build());
+
+        // Configure the Client
+        AsyncClient client = AsyncClient.builder()
+                .region("cn-hangzhou") // Region ID
+                //.httpClient(httpClient) // Use the configured HttpClient, otherwise use the default HttpClient (Apache HttpClient)
+                .credentialsProvider(provider)
+                //.serviceConfiguration(Configuration.create()) // Service-level configuration
+                // Client-level configuration rewrite, can set Endpoint, Http request parameters, etc.
+                .overrideConfiguration(
+                        ClientOverrideConfiguration.create()
+                                .setEndpointOverride("dysmsapi.aliyuncs.com")
+                        //.setConnectTimeout(Duration.ofSeconds(30))
+                )
+                .build();
+
+        // Parameter settings for API request
+        SendSmsRequest sendSmsRequest = SendSmsRequest.builder()
+                .phoneNumbers("15348520652")
+                .signName("周忠明的博客")
+                .templateParam("{\"code\":\"1234\"}")
+                .templateCode("SMS_276347072")
+                // Request-level configuration rewrite, can set Http request parameters, etc.
+                // .requestConfiguration(RequestConfiguration.create().setHttpHeaders(new HttpHeaders()))
+                .build();
+
+        // Asynchronously get the return value of the API request
+        CompletableFuture<SendSmsResponse> response = client.sendSms(sendSmsRequest);
+        // Synchronously get the return value of the API request
+        SendSmsResponse resp = response.get();
+        System.out.println(new Gson().toJson(resp));
+        // Asynchronous processing of return values
+        /*response.thenAccept(resp -> {
+            System.out.println(new Gson().toJson(resp));
+        }).exceptionally(throwable -> { // Handling exceptions
+            System.out.println(throwable.getMessage());
+            return null;
+        });*/
+
+        // Finally, close the client
+        client.close();
+    }
+
+}
